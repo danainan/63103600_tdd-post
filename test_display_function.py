3474807896700e5b3f0a83b4a59da9fe91cfc82d
danainan <71175110+danainan@users.noreply.github.com>
2023-03-27 11:35:15 +0700

    Update test_display_function.py

--- test_display_function.py
+++ test_display_function.py
@@ -8,4 +8,4 @@ def test_display():
     assert display_compensation(30,30,30) == 30*340+30*60+((30-30)*1000)
     assert display_compensation(1,3,0) == 1*340+3*60+((1-0)*1000)
     #ทำงาน 30 วัน ไม่มาสาย 30 วัน ทำ OT 90 ชั่วโมง(ทุกวันวันละ 3 ชั่วโมง)
-    assert display_compensation(30,90,30) == 1*340+3*60+((1-0)*1000)
+    assert display_compensation(30,90,30) == 30*340+90*60+((30-0)*1000)
