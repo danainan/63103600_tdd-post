34a86c6ade879af4b7ab77356dfa85f31d78b2c0
danainan <71175110+danainan@users.noreply.github.com>
2023-03-27 11:40:39 +0700

    Update function.py

--- function.py
+++ function.py
@@ -7,13 +7,13 @@ def inputt():
 
 def validate_number(number_day,number_ot,late_day) :
     if not isinstance(number_day, int) or number_day < 0 or number_day > 30:
-        return False, "number_day must be integer and between 0-30 and not equal 0"
+        return False
     # Validate that number_ot is an integer no more than number_day*3
     if not isinstance(number_ot, int) or number_ot < 0 or number_ot > number_day*3:
-        return False, "number_ot must be integer and no more than number day*3"
+        return False
     # Validate that late_day is an integer no more than number_day
     if not isinstance(late_day, int) or late_day < 0 or late_day > number_day:
-        return False, "late_day must be integer and no more than number day"
+        return False
     return True
 
 def display_compensation(number_day,number_ot,late_day) :
